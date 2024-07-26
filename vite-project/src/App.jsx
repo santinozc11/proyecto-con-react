7965b3b04acb8af0e0745a27daae4b4b4cdc6677
Santino <95001@sistemas.frc.utn.edu.ar>
2024-07-25 21:52:26 -0300

    optimizacion

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,19 +1,19 @@
-import './App.css'
-import About from './Components/About/About';
-import Footer from './Components/Footer/Footer';
-import Header from './Components/Header/Header';
-import Hero from './Components/Hero/Hero';
-import Projects from './Components/Projects/Projects';
-import Services from './Components/Services/Services';
-import SkillyExp from './Components/SkillyExp/SkillyExp';
-import Testimonial from './Components/Testimonial/Testimonial';
+import React, { Suspense, lazy } from 'react';
+import './App.css';
 
+// Importación dinámica
+const About = lazy(() => import('./Components/About/About'));
+const Footer = lazy(() => import('./Components/Footer/Footer'));
+const Header = lazy(() => import('./Components/Header/Header'));
+const Hero = lazy(() => import('./Components/Hero/Hero'));
+const Projects = lazy(() => import('./Components/Projects/Projects'));
+const Services = lazy(() => import('./Components/Services/Services'));
+const SkillyExp = lazy(() => import('./Components/SkillyExp/SkillyExp'));
+const Testimonial = lazy(() => import('./Components/Testimonial/Testimonial'));
 
 function App() {
-  
-
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Header />
       <Hero/>
       <About/>
@@ -22,8 +22,8 @@ function App() {
       <Projects/>
       <Testimonial/>
       <Footer/>
-    </>
-  )
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
